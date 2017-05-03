f4f7f8fd5e5587f6d0a93d1d460bdd9c4fdf4f34
Peter Bouquet <peterbyte@users.noreply.github.com>
2017-05-03 14:18:53 +0100

    Add image path to build, hash and compress site assets

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,8 +4,19 @@ module.exports = function(grunt) {
 	    pkg: grunt.file.readJSON('package.json'),
 	    clean: {
 			deleteTarget: ['target', 'build'],
-			deleteTmpGz: ['target/**/*.gz'],
-			deleteCssTmp: ['target/styles']
+			deleteTmpGz: ['target/**/*.gz']
+	    },
+	    copy: {
+	    	copyImgs: {
+	    		files: [{
+	    			expand: true,
+	    			cwd: 'src',
+	    			src: [
+	    				'img/**/*.*'
+	    			],
+	    			dest: 'target/'
+	    		}]
+	    	}
 	    },
 	    'compile-handlebars': {
 	    	compile: {
@@ -68,25 +79,58 @@ module.exports = function(grunt) {
 				},
 				src: ['target/**/*.html']
 			}
+		},
+		hashres: {
+			options:{
+				fileNameFormat: '${name}.${hash}.${ext}',
+				renameFiles: true
+			},
+			prod:{
+				src:[
+					'target/scripts/**/*.js', 
+					'target/styles/**/*.css',
+					'target/img/**/*.*'
+				],
+				dest: ['target/**/*.html', 'target/**/*.css']
+			}
+		},
+		compress: {
+			main:{
+				options: {
+				  mode: 'gzip'
+				},
+				expand: true,
+				src: ['target/**/*.*']
+			}
 		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-clean');
+	grunt.loadNpmTasks('grunt-contrib-copy');
 	grunt.loadNpmTasks('grunt-compile-handlebars');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-compress');
 	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-sass');
 	grunt.loadNpmTasks('grunt-uncss');
   	grunt.loadNpmTasks('grunt-inline');
+  	grunt.loadNpmTasks('grunt-hashres');
 
 	grunt.registerTask('compile', [
 		'clean:deleteTarget',
+		'copy:copyImgs',
 		'compile-handlebars:compile',
 		'uglify',
 		'sass:compile',
 		'cssmin:fromBuiltSrc',
 		'uncss',
 		'inline'
+	]);
+
+	grunt.registerTask('default', [
+		'compile',
+		'hashres',
+		'compress'
 	]);
 
 };
