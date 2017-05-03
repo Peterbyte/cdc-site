55460a4adcc272eec5f442f3a0d82bcbec4e1986
Peter Bouquet <peterbyte@users.noreply.github.com>
2017-05-03 14:03:01 +0100

    Add style processing

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
 	grunt.initConfig({
 	    pkg: grunt.file.readJSON('package.json'),
 	    clean: {
-			deleteTarget: ['target'],
+			deleteTarget: ['target', 'build'],
 			deleteTmpGz: ['target/**/*.gz'],
 			deleteCssTmp: ['target/styles']
 	    },
@@ -30,17 +30,63 @@ module.exports = function(grunt) {
 	    		src: 'src/scripts/**/*.js',
 	    		dest: 'target/scripts/cdc-site.min.js'
 	    	}
-	    }
+	    },
+	    sass: {
+	    	compile: {
+	    		files: [{
+	    			expand: true,
+	    			cwd: 'src/styles',
+	    			src: ['*.scss'],
+	    			dest: 'build/styles/',
+	    			ext: '.css'
+	    		}]
+	    	}
+	    },
+	    cssmin: {
+			options: {
+				keepSpecialComments: 0
+			},
+			fromBuiltSrc: {
+				files:{
+					'target/styles/cdc-site.min.css': ['build/styles/**/*.css']
+				}
+			}
+		},
+		uncss: {
+			dist: {
+				files: {
+					'target/styles/cdc-site.min.css': [
+						'target/**/*.html'
+					]
+				}
+			}
+		},
+		inline: {
+			dist: {
+				options: {
+					cssmin: true
+				},
+				src: ['target/**/*.html']
+			}
+		}
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-clean');
 	grunt.loadNpmTasks('grunt-compile-handlebars');
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-sass');
+	grunt.loadNpmTasks('grunt-contrib-cssmin');
+	grunt.loadNpmTasks('grunt-uncss');
+  	grunt.loadNpmTasks('grunt-inline');
 
 	grunt.registerTask('compile', [
 		'clean:deleteTarget',
 		'compile-handlebars:compile',
-		'uglify'
+		'uglify',
+		'sass:compile',
+		'cssmin:fromBuiltSrc',
+		'uncss',
+		'inline'
 	]);
 
 };
